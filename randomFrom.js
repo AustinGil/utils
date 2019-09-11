55187089ef645dde9de39916b5b00eb44a36420a
Austin Gil <aussng@gmail.com>
2019-09-11 08:56:46 -0700

    Update randomFrom.js

--- randomFrom.js
+++ randomFrom.js
@@ -1,3 +1,9 @@
+/**
+ * Returns a random value from a given array, given string, or range of numbers.
+ * @param {(number|array|string)} [from=0] The lower bound of a range of numbers, or the source string or array to pull from.
+ * @param {number} [to=100] The upper bound of a range of numbers.
+ * @return {*}
+ */
 function randomFrom(from = 0, to = 100) {
   if (Array.isArray(from) || typeof from === "string") {
     const index = randomFrom(0, from.length - 1)

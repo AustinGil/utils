c71185592e9d91838a088e51023f86c2ba9a5ce0
Austin Gil <aussng@gmail.com>
2019-09-11 08:34:52 -0700

    Update getCookie.js

--- getCookie.js
+++ getCookie.js
@@ -1,3 +1,7 @@
+/**
+ * @param {string} name Name of the cookie value you want to receive.
+ * @return {string} The value of the cookie for the given name
+ */
 function getCookie(name) {
   const cookiesArr = document.cookie.split("; ").map(str => str.split("="))
   const cookiesObj = Object.fromEntries(cookiesArr)

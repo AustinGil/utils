79baabe2cfe35f304346a840a0fc7a4b9df155bb
Austin Gil <aussng@gmail.com>
2019-09-11 08:45:48 -0700

    Create randomString.js

---
+++ randomString.js
@@ -0,0 +1,15 @@
+/**
+ * Generates a random string of the give length made up of the allowed characters.
+ * @param {number} [length=10] - Length of resulting string.
+ * @pram {string} [allowed=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789] - Characters allowed in the results.
+ * @return {string}
+ */
+function randomString(length = 10, allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") {
+  let result = ""
+  for (let i = 0; i < length; i++) {
+    result += allowed.charAt(Math.floor(Math.random() * allowed.length))
+  }
+  return result
+}
+
+export default randomString

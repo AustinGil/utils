52ed58177014ca4d2132c809b4a0cedf13f6eb8d
Austin Gil <aussng@gmail.com>
2019-09-12 13:55:59 -0700

    Update http.js

--- http.js
+++ http.js
@@ -1,25 +1,3 @@
-// Fetch Options signature:
-// {
-//   method: "GET", // POST, PUT, DELETE, etc.
-//   headers: {
-//     // the content type header value is usually auto-set
-//     // depending on the request body
-//     "Content-Type": "text/plain;charset=UTF-8"
-//   },
-//   body: undefined // string, FormData, Blob, BufferSource, or URLSearchParams
-//   referrer: "about:client", // or "" to send no Referer header,
-//   // or an url from the current origin
-//   referrerPolicy: "no-referrer-when-downgrade", // no-referrer, origin, same-origin...
-//   mode: "cors", // same-origin, no-cors
-//   credentials: "same-origin", // omit, include
-//   cache: "default", // no-store, reload, no-cache, force-cache, or only-if-cached
-//   redirect: "follow", // manual, error
-//   integrity: "", // a hash, like "sha256-abcdef1234567890"
-//   keepalive: false, // true
-//   signal: undefined, // AbortController to abort request
-//   window: window // null
-// }
-
 /**
  * Returns an HTTP method wrapper
  * @param {string} method HTTP method

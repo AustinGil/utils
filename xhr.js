9a58df33f510dd1fbb5c2afaf9ace84cb620d3ab
Austin Gil <aussng@gmail.com>
2019-10-04 09:26:28 -0700

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -8,36 +8,19 @@ function xhr(url, opts = {}) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
 
-    // Alternative:
-    // https://gomakethings.com/promise-based-xhr/
-    // xhr.onreadystatechange = function() {
-    //   // Only run if the request is complete
-    //   if (xhr.readyState !== 4) return;
-
-    //   if (xhr.status >= 200 && xhr.status < 300) {
-    //     return resolve(xhr);
-    //   }
-    //   reject({
-    //     status: xhr.status,
-    //     statusText: xhr.statusText
-    //   });
-    // };
-
     xhr.open(opts.method || "get", url)
 
     Object.keys(opts.headers || {}).forEach(key => {
       xhr.setRequestHeader(key, opts.headers[key])
     })
 
-    xhr.onload = e => {
-      return resolve(e.target.responseText)
-    }
-
-    xhr.onerror = reject
-
     if (xhr.upload && opts.onProgress) {
       xhr.upload.onprogress = opts.onProgress
     }
+
+    xhr.onload = e => resolve(e.target.responseText)
+
+    xhr.onerror = reject
 
     xhr.send(opts.body)
   })

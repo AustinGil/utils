275d181ff49d157b6a56af9e13fdab3c2f16e725
Austin Gil <austin@stegosource.com>
2019-11-01 08:10:14 -0700

    Create deferredPromise.js

---
+++ js/deferredPromise.js
@@ -0,0 +1,13 @@
+function deferred() {
+  let res, rej
+
+  const promise = new Promise((resolve, reject) => {
+    res = resolve
+    rej = reject
+  })
+
+  promise.resolve = res
+  promise.reject = rej
+
+  return promise
+}

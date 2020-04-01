95d0d766fd969102d2409578827379928f9b5d1f
Austin Gil <austin@stegosource.com>
2020-04-01 13:46:48 -0700

    Update pluck.js

--- js/pluck.js
+++ js/pluck.js
@@ -1,8 +1,6 @@
-function pluck(obj, keys) {
-  return keys.reduce((next, key) => {
-    next[key] = obj[key]
-    return next
-  }, {})
-}
+const pluck(obj, keys) => keys.reduce((next, key) => {
+  next[key] = obj[key]
+  return next
+}, {})
 
 pluck({ a: 'a', b: 'b'}, ['b']) // { b: 'b' }

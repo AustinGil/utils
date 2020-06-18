ea0106d4e102ba5eebcda88960624913809f973d
Austin Gil <austin@stegosource.com>
2020-06-18 12:09:26 -0700

    Update strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,3 +1,3 @@
 function camelCase(str) {
-    return str.replace(/(_)([A-Za-z0-9])/g, (...parts) => parts[2].toUpperCase())
+    return str.replace(/_([A-Za-z0-9])/g, (...parts) => parts[1].toUpperCase())
 }

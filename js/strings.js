d4fafd14c61584bf5a2a6a6f76735c260f580d82
Austin Gil <austin@stegosource.com>
2020-07-09 07:53:59 -0700

    Update strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,3 +1,19 @@
-function camelCase(str) {
-    return str.replace(/_([A-Za-z0-9])/g, (...parts) => parts[1].toUpperCase())
-}
+/**
+ * Converts a string from snake_case to camelCase
+ * @param {string} str
+ * @return {string}
+ */
+exports.camelCase = (str) => {
+  return str.replace(/_(\w)/g, (_, c) => c.toUpperCase());
+};
+
+/**
+ * Converts a string from camelCase to snake_case
+ * @param {string} str
+ * @return {string}
+ */
+exports.snakeCase = (str) => {
+  return str
+    .replace(/([a-z])([A-Z]+)/g, (...parts) => `${parts[1]}_${parts[2]}`)
+    .toLowerCase();
+};

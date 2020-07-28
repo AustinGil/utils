f3878e1c8f44d8595873afabe5e7d9cfcbdeacb3
Austin Gil <austin@stegosource.com>
2020-07-28 09:02:38 -0700

    Update getRawType.js

--- js/getRawType.js
+++ js/getRawType.js
@@ -5,3 +5,18 @@
 const getRawType = (v) => {
   return Object.prototype.toString.call(v).slice(8, -1).toLowerCase();
 };
+
+/** TESTS
+describe('utils.getRawType()', () => {
+  it('returns the primitive type name for each type', () => {
+    expect(utils.getRawType('yo')).toBe('string');
+    expect(utils.getRawType(42)).toBe('number');
+    expect(utils.getRawType(false)).toBe('boolean');
+    expect(utils.getRawType(() => {})).toBe('function');
+    expect(utils.getRawType([])).toBe('array');
+    expect(utils.getRawType({})).toBe('object');
+    expect(utils.getRawType(null)).toBe('null'); // eslint-disable-line unicorn/no-null
+    expect(utils.getRawType(undefined)).toBe('undefined'); // eslint-disable-line unicorn/no-useless-undefined
+  });
+});
+*/

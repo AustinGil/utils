0fc86d5ffc300229b52b86d547f68b2989685c1c
Austin Gil <austin@stegosource.com>
2020-12-17 05:37:01 -0800

    Update strings.js

--- js/strings.js
+++ js/strings.js
@@ -43,3 +43,27 @@ describe('utils.snakeCase()', () => {
   });
 });
 */
+
+/**
+ * @param {string} needle
+ * @param {string} haystack
+ */
+exports.fuzzysearch = function fuzzysearch(needle, haystack) {
+  const hlen = haystack.length;
+  const nlen = needle.length;
+
+  if (nlen > hlen) return false;
+
+  if (nlen === hlen) return needle === haystack;
+
+  outer: for (let i = 0, j = 0; i < nlen; i++) {
+    const nch = needle.charCodeAt(i);
+    while (j < hlen) {
+      if (haystack.charCodeAt(j++) === nch) {
+        continue outer;
+      }
+    }
+    return false;
+  }
+  return true;
+}

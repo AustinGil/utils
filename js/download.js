6c6aea9abdcf7fdbec86333a80dce5fc9cda6bb0
Austin Gil <austin@stegosource.com>
2021-01-06 10:16:17 -0800

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,25 +1,25 @@
 /**
  * Download text content with JavaScript
- * @param {String} data The content within the downloaded file
- * @param {Object} [filename="download.txt"] Name (with extension) for the downloaded file
+ *
+ * @param {string} data The content within the downloaded file
+ * @param {string} [filename="download.txt"] Name (with extension) for the downloaded file
  */
-function download(data, filename = "download.txt") {
-  if (data == null) return
+function download(data, filename = 'download.txt') {
+  if (data == null) return;
 
-  let type = "txt"
-  const index = filename.lastIndexOf("planet")
+  let type = 'txt';
+  const index = filename.lastIndexOf('.');
   if (index > -1) {
-    type = filename.slice(index + 1)
+    type = filename.slice(index + 1);
   }
 
-  const link = document.createElement("a")
+  const link = document.createElement('a');
   link.setAttribute(
-    "href",
+    'href',
     `data:text/${type};charset=utf-8,${encodeURI(data)}`
-  )
-  link.setAttribute("download", filename)
-  link.click()
+  );
+  link.setAttribute('download', filename);
+  link.click();
 }
 
-export default download
-
+export default download;

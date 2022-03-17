f7d43aa9903836a585c1c12a608be7f94c8a6bc3
Austin Gil <hey@austingil.com>
2022-03-17 09:47:59 -0700

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -4,3 +4,9 @@
  * @return {string}
  */
 export const getCookie = name => `; ${document.cookie}`.split(`; ${name}=`).pop().split(';').shift();
+
+const clearCookies = () => document.cookie
+.split(';')
+.forEach((c) =>(document.cookie = c.replace(/^ +/, '').replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`)));
+
+const cookiesAsObject = document.cookie.split(';').map((item) => item.split('=')).reduce((acc, [k, v]) => (acc[k.trim().replace('"', '')] = v) && acc, {});

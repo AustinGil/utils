9db62717918987325710677d930684b9ffb4eb67
Austin Gil <hey@austingil.com>
2022-04-20 14:15:43 -0700

    Create debounce.js

---
+++ js/debounce.js
@@ -0,0 +1,12 @@
+const debounce = (func, delay, { leading } = {}) => {
+  let timerId
+
+  return (...args) => {
+    if (!timerId && leading) {
+      func(...args)
+    }
+    clearTimeout(timerId)
+
+    timerId = setTimeout(() => func(...args), delay)
+  }
+}

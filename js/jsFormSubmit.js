c335386b589d005eb86b8da189c3c9b1b7de0ed8
Austin Gil <hey@austingil.com>
2023-04-14 11:55:54 -0700

    Create jsFormSubmit.js

---
+++ js/jsFormSubmit.js
@@ -0,0 +1,24 @@
+/** @param {Event} event */
+export function jsFormSubmit(event) {
+  /** @type {HTMLFormElement} */
+  const form = event.currentTarget;
+  const url = new URL(form.action);
+  const formData = new FormData(form);
+  const searchParameters = new URLSearchParams(formData);
+
+  /** @type {Parameters<fetch>[1]} */
+  const fetchOptions = {
+    method: form.method,
+  };
+
+  if (form.method.toLowerCase() === 'post') {
+    fetchOptions.body =
+      form.enctype === 'multipart/form-data' ? formData : searchParameters;
+  } else {
+    url.search = searchParameters;
+  }
+
+  fetch(url, fetchOptions);
+
+  event.preventDefault();
+}

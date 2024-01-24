6b5a3fb5549521c7b961f7bb4de04e7f7b06d142
Austin Gil <hey@austingil.com>
2024-01-24 05:18:15 -0800

    Update commands.sh

--- docker/commands.sh
+++ docker/commands.sh
@@ -1,2 +1,2 @@
-# Build, run, and remove an image when exited
-docker run --name my_app -d -p 3000:3000 --rm $(docker buildx build -q .)
+# Build image `my_app`, then run built image as `my_app`, and remove the container when exited (image `my_app:latest` remains)
+docker run --name my_app -d -p 3000:3000 --rm $(docker buildx build -t my_app:latest -q .)
